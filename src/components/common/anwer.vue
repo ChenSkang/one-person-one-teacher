72bd3af8c6875f0dd36b202be377f2b2fb71e1dd
anknife <1981856494@qq.com>
2018-05-15 21:21:30 +0800

    answer

---
+++ src/components/common/anwer.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <el-dialog title="试题解析" :visible.sync="IFJX" width="70%">
+      <div class="ST TI">{{myTest[0].que}}</div>
+      <div class="KD TI">【考点】{{myTest[0].kddp}}</div>
+      <div class="ZSD TI">【知识点】{{myTest[0].zsd}}</div>
+      <div class="JX TI">【解析】{{myTest[0].answer}}</div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  import bus from '../../bus'
+  export default {
+    data () {
+      return {
+        IFJX: false,
+        myTest: [
+          { que: '' },
+          { kddp: '' },
+          { zsd: '' },
+          { answer: '' }
+        ]
+      }
+    },
+    mounted () {
+      bus.$on('JX', (que, kddp, zsd, answer) => {
+        this.IFJX = true
+        this.myTest[0].que = que
+        this.myTest[0].kddp = kddp
+        this.myTest[0].zsd = zsd
+        this.myTest[0].answer = answer
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .TI{
+    line-height: 25px;
+    font-size: 14px;
+    padding: 20px 20px 20px 20px;
+  }
+</style>
