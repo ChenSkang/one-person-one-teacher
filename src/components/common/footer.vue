14e23682fbf39377b09b7ab129f204597c97e0be
anknife <1981856494@qq.com>
2018-05-15 21:21:24 +0800

    footer

---
+++ src/components/common/footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="footer">
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+#footer{
+  width: 100%;
+  min-width: 1200px;
+  color: #5c5c5c;
+  border-top: 1px solid #DCDFE6;
+  text-align: center;
+  line-height: 30px;
+  padding-top: 10px;
+  background: #fff;
+  height: 91px;
+}
+  .about{
+    font-size: 13px;
+  }
+</style>
