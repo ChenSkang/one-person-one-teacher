c6ad4f0b8378ac194564fa51ca55b3300952b356
anknife <1981856494@qq.com>
2018-05-15 21:19:41 +0800

    based

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style>
+  *{
+    margin:0;
+    padding:0;
+    text-decoration: none;
+    list-style: none;
+    -moz-user-select: none;
+    -khtml-user-select: none;
+    user-select: none;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    width: 100%;
+    height: 100%;
+  }
+</style>
---
+++ src/bus.js
@@ -0,0 +1,5 @@
+/**
+ * Created by getYou on 2018/4/30.
+ */
+import Vue from 'vue'
+export default new Vue()
