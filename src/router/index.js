9c8555fb04a95b0eb8506844c9efdbf8b95f429a
anknife <1981856494@qq.com>
2018-05-16 12:49:17 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -9,12 +9,12 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/Sign',
       name: 'Sign',
       component: Sign
     },
     {
-      path: '/Home',
+      path: '/',
       name: 'Home',
       component: Home
     },
