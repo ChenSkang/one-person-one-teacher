94004a3d7b295bd242062adacadf71012e000245
anknife <1981856494@qq.com>
2018-05-25 11:10:30 +0800

    index

--- src/store/index.js
+++ src/store/index.js
@@ -10,11 +10,7 @@ export default new Vuex.Store({
     tests: '', // test ID
     TK: [], // 试题篮填空题
     XZ: [], // 试题篮选择题
-    JD: [], // 试题篮解答题
-    menu1: true, // 当前路由
-    menu2: false,
-    menu3: false,
-    menu4: false
+    JD: [] // 试题篮解答题
   },
   mutations: {
   }
