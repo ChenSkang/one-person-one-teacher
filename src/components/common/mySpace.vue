e10be0fd8735a79649a71ffeff05baba043f6faa
anknife <1981856494@qq.com>
2018-05-26 22:39:51 +0800

    person

---
+++ src/components/common/mySpace.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="space">
+    <el-tooltip class="item" effect="light" content="点击进入试题篮" placement="bottom-start">
+      <el-button icon="el-icon-document" @click="$router.push('/basket')" circle></el-button>
+    </el-tooltip>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+#space{
+  position: absolute;
+  top: 30px;
+  right: 10%;
+}
+</style>
