46db72ca59ae8a1258037373c0f97d49ad9eda7f
anknife <1981856494@qq.com>
2018-05-26 22:39:44 +0800

    header

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="head">
-    <div class="top">
-      <div class="row">
-        <div class="logo">OPOT<span>.</span></div>
-      </div>
+    <el-row class="top">
+      <el-col :span="4" class="row">
+        <div class="logo" @click="$router.push('/')">OPOT<span>.</span></div>
+      </el-col>
+      <!--
       <div class="menu">
         <el-menu :default-active="$route.path"
                  active-text-color="#000" class="el-menu-demo" mode="horizontal" router>
           <el-menu-item index="/">首页</el-menu-item>
           <el-menu-item index="/index">推题</el-menu-item>
-          <el-menu-item index="/basket">试题篮</a></el-menu-item>
+          <el-menu-item index="/basket">试题篮</el-menu-item>
         </el-menu>
-      </div>
-    </div>
+      </div> !-->
+    </el-row>
   </div>
 </template>
 
@@ -30,7 +31,6 @@
 <style scoped>
   .head{
     width: 100%;
-    min-width: 1200px;
     padding: 28px 0;
     background: #fff;
   }
@@ -38,19 +38,16 @@
     width: 100%;
     height: 55px;
     line-height: 55px;
-    display: flex;
-    flex-direction: row;
   }
   .row{
     position: relative;
     left: 12%;
     padding-left: 15px;
     padding-right: 15px;
-    width: 16.66667%;
   }
   .logo{
     color: #000;
-    font-size: 32px;
+    font-size: 2rem;
     margin: 0;
     padding: 0;
     font-weight: 700;
@@ -59,6 +56,7 @@
   .logo > span {
     color: #DD356E;
   }
+  /*
   .menu{
     position: absolute;
     right: 12%;
@@ -72,5 +70,5 @@
   .el-menu--horizontal>.el-menu-item.is-active {
     border-bottom: 2px solid #DD356E !important;
     color: #303133;
-  }
+  } */
 </style>
