c0cbfbb1c4f23c23bffdd7dd47a3c4c4630a63d7
anknife <1981856494@qq.com>
2018-08-01 22:29:53 +0800

    解析样式

--- src/components/common/anwer.vue
+++ src/components/common/anwer.vue
@@ -2,10 +2,10 @@
   <div id="answer">
     <el-dialog title="试题解析" :visible.sync="IFJX" width="70%">
       <div class="ST TI" v-html="myTest[0].que"></div>
-      <div class="ZSD TI" v-if="myTest[0].zsd">【考点】<span v-html="myTest[0].zsd"></span></div>
-      <div class="JX TI">【解析】<span v-html="myTest[0].jx"></span></div>
-      <div class="JX TI">【解答】<span v-html="myTest[0].answer"></span></div>
-      <div class="KD TI" v-if="myTest[0].kddp">【点评】<span v-html="myTest[0].kddp"></span></div>
+      <div class="ZSD TI" v-if="myTest[0].zsd"><span class="jx">考点：</span><span v-html="myTest[0].zsd"></span></div>
+      <div class="JX TI"><span class="jx">解析：</span><span v-html="myTest[0].jx"></span></div>
+      <div class="JX TI"><span class="jx">解答：</span><span v-html="myTest[0].answer"></span></div>
+      <div class="KD TI" v-if="myTest[0].kddp"><span class="jx">点评：</span><span v-html="myTest[0].kddp"></span></div>
     </el-dialog>
   </div>
 </template>
@@ -45,7 +45,11 @@
   .TI{
     line-height: 25px;
     font-size: 14px;
-    padding: 20px 20px 20px 20px;
+    padding: 10px 20px 10px 20px;
     letter-spacing: 1px;
+  }
+  .jx{
+    color: #409EFF;
+    font-weight: bold;
   }
 </style>
