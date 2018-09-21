6e871608edbca771b116694a83326945d2361b6d
anknife <1981856494@qq.com>
2018-09-21 16:02:59 +0800

    head

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -4,15 +4,6 @@
       <el-col :span="4" class="row">
         <div class="logo" @click="$router.push('/')">OPOT<span>.</span></div>
       </el-col>
-      <!--
-      <div class="menu">
-        <el-menu :default-active="$route.path"
-                 active-text-color="#000" class="el-menu-demo" mode="horizontal" router>
-          <el-menu-item index="/">首页</el-menu-item>
-          <el-menu-item index="/index">推题</el-menu-item>
-          <el-menu-item index="/basket">试题篮</el-menu-item>
-        </el-menu>
-      </div> !-->
     </el-row>
   </div>
 </template>
@@ -30,30 +21,28 @@
 
 <style scoped>
   .head{
+    z-index: 999;
+    position: fixed;
     width: 100%;
-    padding: 28px 0;
-    background: #fff;
+    height: 40px;
+    background-image: url("./../../img/base_title.png");
+    background-size: cover;
   }
   .top {
     width: 100%;
-    height: 55px;
-    line-height: 55px;
+    height: 40px;
+    line-height: 40px;
   }
   .row{
     position: relative;
-    left: 12%;
-    padding-left: 15px;
-    padding-right: 15px;
+    left: 16%;
   }
   .logo{
-    color: #000;
-    font-size: 2rem;
+    color: #fff;
+    font-size: 25px;
     margin: 0;
     padding: 0;
     font-weight: 700;
     font-family: "Inconsolata", Arial, sans-serif;
-  }
-  .logo > span {
-    color: #DD356E;
   }
 </style>
