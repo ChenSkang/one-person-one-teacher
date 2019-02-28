a60ccd77bb3a73bfc52d40c25b1d408dfafe985c
ChenSkang <1981856494@qq.com>
2019-02-28 17:28:29 +0800

    update

--- src/components/common/anwer.vue
+++ src/components/common/anwer.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="answer">
     <el-dialog title="试题解析" :visible.sync="$store.state.IFJX" width="70%" :append-to-body="true">
-      <div class="ST TI" v-html="$store.state.myTest[0].que"></div>
+      <div class="ST TI" v-html="$store.state.myTest[0].question"></div>
       <div class="ZSD TI" v-if="$store.state.myTest[0].zsd"><span class="jx">考点：</span><span v-html="$store.state.myTest[0].zsd"></span></div>
-      <div class="JX TI"><span class="jx">解析：</span><span v-html="$store.state.myTest[0].jx"></span></div>
+      <div class="JX TI"><span class="jx">解析：</span><span v-html="$store.state.myTest[0].analysis"></span></div>
       <div class="JX TI"><span class="jx">解答：</span><span v-html="$store.state.myTest[0].answer"></span></div>
       <div class="KD TI" v-if="$store.state.myTest[0].kddp"><span class="jx">点评：</span><span v-html="$store.state.myTest[0].kddp"></span></div>
     </el-dialog>
