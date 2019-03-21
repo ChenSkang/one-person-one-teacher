0b630de445301fe70a714844a4d1ebfd5eb31204
ChenSkang <1981856494@qq.com>
2019-03-21 11:05:19 +0800

    我的试卷修改

--- src/components/page/TestPapers.vue
+++ src/components/page/TestPapers.vue
@@ -5,14 +5,19 @@
     <div>
       <div class="main" :style="{minHeight: minHeight + 'px'}">
         <div class="papers" v-if="$store.state.paperList.length">
-          <div class="papers-col" v-for="i in $store.state.paperList.length">
-            {{$store.state.paperList[i - 1].title}}
-            <div @click="getPapert($store.state.paperList[i - 1].id)">查看</div>
-            <div @click="deletePapert($store.state.paperList[i - 1].id)">删除</div>
+          <div class="papers-col" v-for="(value,index) in $store.state.paperList">
+            <div class="file">
+
+            </div>
+            <div class="paper-title">{{value.title}}</div>
+            <div class="paper-foot">
+              <div class="paper-foot-div"><div @click="getPapert(value.id, value.title)" class="paper-foot-btn">查看</div></div>
+              <div class="paper-foot-div"><div @click="deletePapert(value.id)" class="paper-foot-btn">删除</div></div>
+            </div>
           </div>
         </div>
-        <div class="table-delete"><el-button size="small" type="primary" @click="newPaper()">创建新的试卷</el-button></div>
       </div>
+      <div class="table-delete"><el-button size="small" type="primary" @click="newPaper()">创建新的试卷</el-button></div>
     </div>
     <myFoot></myFoot>
   </div>
@@ -50,8 +55,13 @@
           })
         })
       },
-      getPapert (val) {
-        this.getPaper(val)
+      getPapert (pid, title) {
+        let routeData = this.$router.resolve({
+          path: '/basket',
+          query: { title: title, paperId: pid }
+        })
+        window.open(routeData.href, '_blank')
+        this.getPaper(pid)
       },
       deletePapert (val) {
         this.deletePaper(val)
@@ -70,15 +80,70 @@
     position: relative;
     top: 50px;
     margin-bottom: 60px;
+    background-color: #fbfbfb
   }
   .papers{
     width: 90%;
-    margin-left: 5%;
+    margin-left: 8%;
   }
   .papers-col{
+    position: relative;
     float: left;
-    width: 18%;
+    width: 15%;
+    margin-right: 4%;
+    margin-top: 25px;
+    cursor: pointer;
+    box-sizing: border-box;
+    text-align: center;
+    border: 1px solid #fff;
   }
   .table-delete{
+  }
+  .file{
+    width: 80%;
+    height: 100px;
+    margin: 20px 10% 80px 10%;
+    background-color: #fff;
+    box-sizing: border-box;
+    border: 1px solid #C0C4CC;
+    border-radius: 3px;
+  }
+  .paper-title{
+    width: 100%;
+    position: absolute;
+    bottom: 40px;
+    height: 40px;
+    line-height: 40px;
+    text-align: center;
+    font-size: 14px;
+  }
+  .paper-foot{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 40px;
+    background-color: #fff;
+    display: flex;
+    flex-direction: row;
+  }
+  .paper-foot-div{
+    width: 50%;
+    margin: 5px auto;
+  }
+  .paper-foot-btn{
+    width: 50px;
+    height: 28px;
+    line-height: 28px;
+    font-size: 14px;
+    color: #fff;
+    border: 1px solid #fff;
+    border-radius: 3px;
+    margin: 0 auto;
+  }
+  .papers-col:hover{
+    border: 1px solid #2d8cf0;
+  }
+  .papers-col:hover .paper-foot{
+    background-color: #2d8cf0;
   }
 </style>
