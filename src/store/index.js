b997d1edc17e5885898df4032783906c00ac320b
ChenSkang <1981856494@qq.com>
2019-03-22 13:02:07 +0800

    关于个人信息的修改

--- src/store/index.js
+++ src/store/index.js
@@ -15,26 +15,11 @@ export default new Vuex.Store({
     nowSub: [],
     nowSubs: '',
     userNow: '',
+    imgSrc: '', // 头像
     cropImg: '', // 裁切的图片
     TK: [], // 试题篮填空题
     XZ: [], // 试题篮选择题
     JD: [], // 试题篮解答题
-    /* options: [{
-      value: 0,
-      label: '题干',
-      holder: '请输入题干'
-    }, {
-      value: 1,
-      label: '试卷',
-      holder: '请输入中考试卷信息，例：2017潍坊'
-    }, {
-      value: 2,
-      label: '知识点',
-      holder: '请在左侧选择知识点'
-    }],
-    value: 0,
-    select: '全部',
-    zsdTreeTags: [], */
     input_message: '',
     myTest: [
       { question: '' },
