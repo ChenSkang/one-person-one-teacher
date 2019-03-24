3f6bf8ef3401e4f6bf3975a50d091be12beef24f
ChenSkang <1981856494@qq.com>
2019-03-24 22:07:57 +0800

    sign

--- src/components/common/Sign.vue
+++ src/components/common/Sign.vue
@@ -188,7 +188,15 @@
         let num = Math.random() * 10000
         let routeData = this.$router.resolve({
           path: '/index',
-          query: {servlet: 'wordSearch', msg: this.$store.state.input_message, page: 1, num: num}
+          query: {
+            servlet: 'wordSearch',
+            msg: this.$store.state.input_message,
+            page: 1,
+            kind: '全部',
+            nianji: this.$store.state.nianji,
+            jiaocai: this.$store.state.jiaocai,
+            num: num
+          }
         })
         window.open(routeData.href, '_blank')
       },
@@ -196,7 +204,15 @@
         let num = Math.random() * 10000
         let routeData = this.$router.resolve({
           path: '/index',
-          query: {servlet: 'wordSearch', msg: hot, page: 1, num: num}
+          query: {
+            servlet: 'wordSearch',
+            msg: hot,
+            page: 1,
+            kind: '全部',
+            nianji: this.$store.state.nianji,
+            jiaocai: this.$store.state.jiaocai,
+            num: num
+          }
         })
         window.open(routeData.href, '_blank')
       },
