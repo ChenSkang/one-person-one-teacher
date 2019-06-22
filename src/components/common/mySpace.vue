2d6a00a40a8b4e49c8fdb6326ef3010cf9cc4490
ChenSkang <1981856494@qq.com>
2019-06-22 19:39:55 +0800

    鼠标样式

--- src/components/common/mySpace.vue
+++ src/components/common/mySpace.vue
@@ -67,14 +67,14 @@
         <p>已经有账号，马上去<span class="to" @click="gosign()">登录</span></p>
       </el-form>
     </el-dialog>
-    <el-dialog :title="msg" :visible.sync="$store.state.signShow" width="30%" :append-to-body="true">
+    <el-dialog :title="msg" :visible.sync="$store.state.signShow" width="30%" :close-on-click-modal="false" :append-to-body="true">
       <el-form :model="signForm" :rules="signRule" ref="signForm" class="demo-ruleForm">
         <el-form-item prop="usr">
           <el-input type="text" v-model="signForm.usr" auto-complete="off" placeholder="手机/用户名"></el-input>
         </el-form-item>
         <el-form-item prop="pass">
           <el-input :type="pswd" v-model="signForm.pass" @keyup.native.enter="submitSignForm('signForm')" placeholder="密码">
-            <i slot="suffix" class="el-input__icon el-icon-view" @click="changePs(pswd)"></i>
+            <i slot="suffix" style="cursor: pointer" class="el-input__icon el-icon-view" @click="changePs(pswd)"></i>
           </el-input>
         </el-form-item>
         <el-form-item>
